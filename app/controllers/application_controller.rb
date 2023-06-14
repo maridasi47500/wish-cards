de5f094f3fc6be564823d6295e894165da189773
CJeanne ANonym <anonyme201504@gmail.com>
2023-06-14 11:22:18 -0300

    actioncable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   before_action :myaction 
   def myaction
     cookies.encrypted[:user_id]=current_user.try(:id)
