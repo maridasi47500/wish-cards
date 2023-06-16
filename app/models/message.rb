49a4e30d10efe273904c9412d10a266522c20e0d
CJeanne ANonym <anonyme201504@gmail.com>
2023-06-16 11:11:15 -0300

    mytextsandpics

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,8 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :room
+  has_many :pics
+  accepts_nested_attributes_for :pics, allow_destroy: true
   has_many :texts
   accepts_nested_attributes_for :texts, allow_destroy: true
   def image=(uploaded_io)
