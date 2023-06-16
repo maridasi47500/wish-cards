52966357572d1bceaf319a08baed484989cdc190
CJeanne ANonym <anonyme201504@gmail.com>
2023-06-16 10:54:09 -0300

    addpics

--- app/javascript/controllers/rooms_controller.js
+++ app/javascript/controllers/rooms_controller.js
@@ -19,8 +19,31 @@
      }
 
      // When the user clicks anywhere outside of the modal, close it
+// Get the modal
+ var modalpic = document.getElementById("myModalpic");
+
+ // Get the button that opens the modal
+ var btnpic = document.getElementById("myBtnpic");
+
+ // Get the <span> element that closes the modal
+ var spanpic = document.getElementsByClassName("closepic")[0];
+
+ // When the user clicks on the button, open the modal
+ btnpic.onclick = function() {
+   modalpic.style.display = "block";
+   }
+
+   // When the user clicks on <span> (x), close the modal
+   spanpic.onclick = function() {
+     modalpic.style.display = "none";
+     }
+
+     // When the user clicks anywhere outside of the modal, close it
      window.onclick = function(event) {
        if (event.target == modal) {
            modal.style.display = "none";
+             }
+       if (event.target == modalpic) {
+           modalpic.style.display = "none";
              }
              } 
